5ffb444565af2ee70a8e0875b6b567ddf7debb42
Andrey Koryakin <football2331lm@gmail.com>
2020-10-10 11:37:29 +0300

    Tests added

--- src/test/java/ru/netology/domain/ConditionerTest.java
+++ src/test/java/ru/netology/domain/ConditionerTest.java
@@ -51,4 +51,29 @@ public class ConditionerTest {
         conditioner.setCurrentTemperature(currentTemperature);
         assertEquals(expectedCurrentTemperature, conditioner.getCurrentTemperature());
     }
+
+    @Test
+    public void increaseCurrentTemperatureEqual() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(5);
+        conditioner.setMaxTemperature(20);
+        int currentTemperature = 20;
+        int expectedCurrentTemperature = 20;
+        conditioner.setCurrentTemperature(currentTemperature);
+        conditioner.increaseCurrentTemperature();
+        assertEquals(expectedCurrentTemperature, conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void decreaseCurrentTemperatureEqual() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setMinTemperature(10);
+        conditioner.setMaxTemperature(35);
+        int currentTemperature = 0;
+        int expectedCurrentTemperature = 0;
+        conditioner.setCurrentTemperature(currentTemperature);
+        conditioner.decreaseCurrentTemperature();
+        assertEquals(expectedCurrentTemperature, conditioner.getCurrentTemperature());
+    }
+
 }
